81625e8053b96c5abb0bc862f61f0d5a1ee9161f
Joosep Pata <joosep.pata@gmail.com>
2018-11-21 13:32:27 -0800

    use LCG

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@
 #eval `scram setenv -sh`
 #cd ..
 
-export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/tbb/2018_U3-16953/x86_64-centos7-gcc62-opt/lib/:$LD_LIBRARY_PATH
-source /cvmfs/sft.cern.ch/lcg/releases/gcc/6.2.0/x86_64-centos7/setup.sh
+#export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/tbb/2018_U3-16953/x86_64-centos7-gcc62-opt/lib/:$LD_LIBRARY_PATH
 #source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-820c6/x86_64-centos7-gcc62-opt/bin/thisroot.sh
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.14.04/x86_64-centos7-gcc48-opt/root/bin/thisroot.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc62-opt/setup.sh
